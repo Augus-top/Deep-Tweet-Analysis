2dd7e9d3e9bf95b4c82a9e6ff45044ee7e79b4b2
Augus-top <acpluschkat@hotmail.com>
2018-11-19 16:47:12 -0200

    routes for the model

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.send('<h1>Deep Learning Analysis</h1>');
+});
+
+router.get('/model2/:name', (req, res) => {
+  res.sendFile(path.join(__dirname, '../models/' + req.params.name));
+});
+
+router.use((error, req, res, next) => {
+  res.status(404).json({
+    type: 'FileNotFound',
+    message: req.originalUrl + ' was not found'
+  });
 });
 
 module.exports = router;
