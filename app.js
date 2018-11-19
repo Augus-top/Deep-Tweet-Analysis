2920cf895353eee46315f817c16f8392f903242b
Augus-top <acpluschkat@hotmail.com>
2018-11-19 19:50:21 -0200

    Cors

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
 
 const app = express();
+
 const port = process.env.PORT || '3000';
 
 app.set('views', path.join(__dirname, 'views'));
-
+app.use(cors());
+app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
